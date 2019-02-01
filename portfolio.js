edb006fdf538dc7e293880ef7981f3c3b935ebee
wekotsai <42733086+wekotsai@users.noreply.github.com>
2019-02-01 23:02:17 +0100

    navbar added

--- portfolio.js
+++
@@ -1,9 +0,0 @@
-// function toggleMenu() {
-//     var tabs = document.getElementById('menu-box');    
-//     if(tabs.style.display == "none") { 
-//         tabs.style.display = "block";
-//     }
-//     else { 
-//         tabs.style.display = "none";
-//     }
-//   }
